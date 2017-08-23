d7fc46d9f06a50abaf21df035725a4c7e3abe6a4
Giovane Boaviagem <giovanebribeiro@gmail.com>
2017-08-23 01:12:16 -0300

    Create the initial structure, and first test cases.

--- test/index.js
+++ test/index.js
@@ -1,128 +1,74 @@
-(function(){
-  "use strict";
+'use strict';
 
-  var Hapi = require("hapi");
-  var Code = require("code");
-  var Lab = require("lab");
-  var hapiCollectionJson = require("../lib/index");
+var Hapi = require('hapi');
+var Code = require('code');
+var Lab = require('lab');
+var debug = require('debug')('hapi-collection-json:test/index.js');
 
-  var lab = exports.lab = Lab.script();
-  var expect = Code.expect;
-  var experiment = lab.experiment;
-  var test = lab.test;
-  var before = lab.before;
-  var after = lab.after;
+var lab = exports.lab = Lab.script();
+var expect = Code.expect;
+var experiment = lab.experiment;
+var test = lab.test;
+var before = lab.before;
+//var after = lab.after;
 
-  var server;
-  var dataTest;
-  experiment("hapi-collection-json tests", function(){
-
+var server;
+//var dataTest;
+experiment('hapi-collection-json tests', function(){
     // create a micro server for tests
     before(function(done){
-      server = new Hapi.Server();
-      server.connection();
-      server.ext("onPreResponse", hapiCollectionJson);
-      dataTest = {}; // represents a data set.
+        server = new Hapi.Server();
+        server.connection();
 
-      // Create
-      server.route({
-        method: "POST",
-        path: "/data",
-        handler: function(request, reply){
-          var code = request.payload.code; // uuid
-          var name = request.payload.name; //string
-          var age = request.payload.age; // number
-          var gender = request.payload.gender; // boolean
+        //put routes here
+        server.route({
+            method: 'GET',
+            path: '/data',
+            config: {
+                handler: function(request, reply){
+                    reply();
+                }
+            }
+        });
 
-          var obj = {
-            name: name,
-            age: age,
-            gender: gender
-          };
-
-          dataTest[code] = obj;
-
-          reply();
-        }
-      });
-
-      // Retrieve
-      server.route({
-        method: "GET",
-        path: "/data/{code}",
-        handler: function(request, reply){
-          var code = request.params.code;
-
-          var obj = dataTest[code];
-
-          reply(obj);
-        }
-      });
-
-      // Retrieve all
-      server.route({
-        method: "GET",
-        path: "/data",
-        handler: function(request, reply){
-          reply(dataSet);
-        }
-      });
-
-
-      // Update
-      server.route({
-        method: "PUT",
-        path: "/data/{code}",
-        handler: function(request, reply){
-          var code = request.params.code; // uuid
-          var name = request.payload.name; //string
-          var age = request.payload.age; // number
-          var gender = request.payload.gender; // boolean
-
-          var obj = {
-            name: name,
-            age: age,
-            gender: gender
-          };
-
-          dataTest[code] = obj;
-
-          reply();
-        }
-      });
-
-      // Delete
-      server.route({
-        method: "DELETE",
-        path: "/data/{code}",
-        handler: function(request, reply){
-          delete dataTest[request.params.code];
-          reply();
-        }
-      });
-
-      done();
+        server.register(require('../lib/index.js'), function(err){
+            if(err) throw err;
+            done();
+        });
     });
 
-    test("Create data", function(done){
-      var request = {
-        method: "POST",
-        url: "/data",
-        payload: {
-          code: "D01",
-          age: 10,
-          gender: true,
-          name: "Alice"
-         }
-      };
-
-      server.inject(request, function(response){
-        expect(response.statusCode).to.equal(200);
-        done();
-      });
-       
+    test('check content-type: wrong one', function(done){
+        server.inject({
+            method: 'GET',
+            url: '/data',
+            headers: {
+                'content-type': 'application/json'
+            }
+        }, function(response){
+            debug(response.result);
+            expect(response.statusCode).to.be.equal(500);
+            expect(response.result.collection).to.exists();
+            expect(response.result.collection.error).to.exists();
+            expect(response.result.collection.error.title).to.exists().and.to.be.equal('Internal Server Error');
+            expect(response.result.collection.error.code).to.exists().and.to.be.equal(500);
+            expect(response.result.collection.error.message).to.exists().and.to.be.equal('Invalid content type. Only allowed: application/vnd.collection+json');
+            done();
+        });
     });
 
-  });
-
-})();
+    test('check content-type', function(done){
+        server.inject({
+            method: 'GET',
+            url: '/data',
+            headers: {
+                'content-type': 'application/vnd.collection+json'
+            }
+        }, function(response){
+            expect(response.statusCode).to.be.equal(200);
+            expect(response.result.collection).to.exists();
+            expect(response.result.collection.version).to.exists().and.to.be.equal('1.0');
+            expect(response.result.collection.href).to.exists();
+            done();
+        });
+    });
+});
